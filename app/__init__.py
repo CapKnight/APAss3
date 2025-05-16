f8d6a56f0c28557790bcae5e1ecc694e518b9917
YUCHEN LI <y.li.24@abdn.ac.uk>
2025-05-16 10:40:45 +0000

    modify for deploy

--- app/__init__.py
+++ app/__init__.py
@@ -3,29 +3,26 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+app = Flask(__name__, static_folder='../static', template_folder='templates')  # 直接定义 app
 
-def create_app():
-    app = Flask(__name__, static_folder='../static', template_folder='templates')
-    app.secret_key = os.getenv('SECRET_KEY', 'your_secret_key')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///characters.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = os.getenv('SECRET_KEY', 'your_secret_key')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///characters.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    db.init_app(app)
+db.init_app(app)
 
-    from app.blueprints.character import character_bp
-    app.register_blueprint(character_bp)
+from app.blueprints.character import character_bp
+app.register_blueprint(character_bp)
 
-    @app.errorhandler(404)
-    def not_found_error(error):
-        return render_template('errors/404.html'), 404
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
 
-    @app.errorhandler(500)
-    def internal_error(error):
-        db.session.rollback()
-        return render_template('errors/500.html'), 500
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('errors/500.html'), 500
 
-    with app.app_context():
-        if os.getenv('FLASK_ENV') == 'development':
-            db.create_all()
-
-    return app
+with app.app_context():
+    if os.getenv('FLASK_ENV') == 'development':
+        db.create_all()
